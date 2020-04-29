71b48fdaf41cd209e89976989576008b442e698d
dandelionadia <nadia.ridko@gmail.com>
2020-04-29 18:32:57 +0200

    adds product images support

--- src/api.js
+++ src/api.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const products = require("../products.json");
 
 const app = express();
 const router = express.Router();
+
+const resolveImages = (product) => {
+  return {
+    ...product,
+    images: product.images.map((image) => {
+      return `https://auros-api.netlify.app/.netlify/functions/api/product/${product.id}/images/${image}`;
+    }),
+  };
+};
 
 router.get("/products", (req, res) => {
   const { id } = req.query;
@@ -16,18 +27,19 @@ router.get("/products", (req, res) => {
 
   const idList = id.split(",");
 
-  const foundProducts = idList.map(productId => {
-    return products.find(product => {
+  const foundProducts = idList.map((productId) => {
+    return products.find((product) => {
       return productId === product.id;
     });
   });
+
   res.json(foundProducts);
 });
 
 router.get("/product/:productId", (req, res) => {
   const productId = req.params.productId;
 
-  const foundProduct = products.find(product => {
+  const foundProduct = products.find((product) => {
     return productId === product.id;
   });
 
@@ -35,10 +47,25 @@ router.get("/product/:productId", (req, res) => {
     return res.status(404).end();
   }
 
-  res.send(foundProduct);
+  res.send(resolveImages(foundProduct));
+});
+
+router.get("/product/:productId/images/:imageName", (req, res) => {
+  const { productId, imageName } = req.params;
+
+  const image = fs.readFileSync(
+    path.resolve(`static/images/${productId}/${imageName}`)
+  );
+
+  res.send(Buffer.from(image));
 });
 
 app.use(cors());
 app.use("/.netlify/functions/api", router);
 
+app.get("/.netlify/functions/files", (req, res) => {
+  res.send("hello to you, handsome");
+});
+
+module.exports = app;
 module.exports.handler = serverless(app);
